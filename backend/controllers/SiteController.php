a74d3208895b9f70028020cb8df3faa3f82a9680
Miriskava <miriskava4@gmail.com>
2019-12-08 22:30:31 +0400

    tt

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -129,7 +129,7 @@ class SiteController extends Controller
 
     public function actionContentdelete($id)
     {
-        $file=$this->findContent($id);
+        $file=$this->findContent($id);$file->delete();
         if(file_exists('https://vipvolpi.host/admin/resource/'.$file->way))
         {unlink('https://vipvolpi.host/admin/resource/'.$file->way);
             $file->delete();}
