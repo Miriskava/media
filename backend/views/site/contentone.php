c6fe54493d9da46bb0dc459832f362c9bbad6cbc
Miriskava <miriskava4@gmail.com>
2019-12-08 21:59:08 +0400

    last change, i hope

--- backend/views/site/contentone.php
+++ backend/views/site/contentone.php
@@ -14,13 +14,13 @@ $this->title = $model->name;
     {
     ?>
         <audio controls>
-            <source src="<?=\Yii::getAlias('@backend').'/web/resource/'.$model->way?>" type="audio/mpeg">
+            <source src="https://vipvolpi.host/admin/resource/<?=$model->way?>" type="audio/mpeg">
         </audio>
     <?php }
     if($model->type=='Video')
     {?>
     <video width="400" height="300" controls="controls" >
-        <source src="<?=\Yii::getAlias('@backend').'/web/resource/'.$model->way?>" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+        <source src="https://vipvolpi.host/admin/resource/<?=$model->way?>" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
     </video>
     <?php }?>
     <div><?=Html::a('Вернуться',['content'],['class'=>'btn btn-info'])?></div>
