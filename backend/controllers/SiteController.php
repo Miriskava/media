1c80871a2214e23390aabf4a185ee38fd53d42a0
Miriskava <miriskava4@gmail.com>
2019-12-08 23:21:41 +0400

    fffsfsfssawe3r4yhk

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -129,12 +129,12 @@ class SiteController extends Controller
 
     public function actionContentdelete($id)
     {
-        $file=$this->findContent($id);return \Yii::getAlias('@backend').'/web/resource/'.$file->way;
-       /* if(file_exists(\Yii::getAlias('@backend/web').'resource/'.$file->way))
+        $file=$this->findContent($id);
+        if(file_exists(\Yii::getAlias('@backend/web').'resource/'.$file->way))
         {unlink(\Yii::getAlias('@backend/web').'resource/'.$file->way);
-            $file->delete();}*/
+            $file->delete();}
 
-       // return $this->redirect(['content']);
+        return $this->redirect(['content']);
     }
 
     protected function findContent($id)
